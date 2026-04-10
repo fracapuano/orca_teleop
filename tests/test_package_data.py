b82c66383a23641e170987b933890f4af7c624b7
Francesco Capuano <fc.francescocapuano@gmail.com>
2026-04-10 15:29:04 +0200

    remove: useless test

--- tests/test_package_data.py
+++
@@ -1,9 +0,0 @@
-from importlib import resources
-
-
-def test_package_data_files_are_shipped():
-    mediapipe_dir = resources.files("orca_teleop.orca_ingress.mediapipe")
-    retargeting_dir = resources.files("orca_teleop.retargeting")
-
-    assert (mediapipe_dir / "hand_landmarker.task").is_file()
-    assert (retargeting_dir / "retargeter.yaml").is_file()
