80ca9fa0d78220736d13c0c94b81fd4c4cf28292
Francesco Capuano <fc.francescocapuano@gmail.com>
2026-04-11 20:00:56 +0200

    simplified ingress

--- src/orca_teleop/ingress/__init__.py
+++ src/orca_teleop/ingress/__init__.py
@@ -1,17 +1,9 @@
 """Ingress layer for ORCA teleoperation."""
 
 from orca_teleop.ingress.server import DEFAULT_PORT, HandLandmarks, IngressServer
-from orca_teleop.ingress.utils import (
-    get_canonical_key_vectors,
-    preprocess_avp_data,
-    preprocess_mediapipe_data,
-)
 
 __all__ = [
     "DEFAULT_PORT",
     "HandLandmarks",
     "IngressServer",
-    "get_canonical_key_vectors",
-    "preprocess_avp_data",
-    "preprocess_mediapipe_data",
 ]
